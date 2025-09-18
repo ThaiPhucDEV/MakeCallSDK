ec3b5d3af125fd42992d4fb9c8a50ba705979799
Phuc Nguyen <phuc@example.com>
2025-09-18 12:05:36 +0700

    Initial Commit

---
+++ MakeCallSDKFramework/MakeCallSDKFramework.swift
@@ -0,0 +1,9 @@
+//
+//  MakeCallSDKFramework.swift
+//  MakeCallSDKFramework
+//
+//  Created by PHUC on 18/9/25.
+//
+
+import Foundation
+
---
+++ MakeCallSDKFrameworkTests/MakeCallSDKFrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  MakeCallSDKFrameworkTests.swift
+//  MakeCallSDKFrameworkTests
+//
+//  Created by PHUC on 18/9/25.
+//
+
+import Testing
+@testable import MakeCallSDKFramework
+
+struct MakeCallSDKFrameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
